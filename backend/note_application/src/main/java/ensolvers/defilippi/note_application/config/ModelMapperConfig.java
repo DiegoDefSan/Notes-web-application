3be860f41488cfc75333b46a300ad3d07e60e838
DiegoDefSan <defsan35@gmail.com>
2024-01-15 22:39:51 -0500

    Add ModelMapper confg

--- backend/note_application/src/main/java/ensolvers/defilippi/note_application/config/ModelMapperConfig.java
+++ backend/note_application/src/main/java/ensolvers/defilippi/note_application/config/ModelMapperConfig.java
@@ -1,2 +1,14 @@
-package ensolvers.defilippi.note_application.config;public class ModelMapperConfig {
+package ensolvers.defilippi.note_application.config;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ModelMapperConfig {
+
+    @Bean
+    public ModelMapper modelMapper() {
+        return new ModelMapper();
+    }
 }
