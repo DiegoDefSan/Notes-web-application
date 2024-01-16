4233c7350c66d8607b7c2a9f9e5e8211e206b7f0
DiegoDefSan <defsan35@gmail.com>
2024-01-15 22:32:35 -0500

    Add create note request dto

--- backend/note_application/src/main/java/ensolvers/defilippi/note_application/dto/note/commands/create_note/CreateNoteRequest.java
+++ backend/note_application/src/main/java/ensolvers/defilippi/note_application/dto/note/commands/create_note/CreateNoteRequest.java
@@ -1,2 +1,11 @@
-package ensolvers.defilippi.note_application.dto.note.commands.create_note;public record CreateNoteRequest() {
+package ensolvers.defilippi.note_application.dto.note.commands.create_note;
+
+import ensolvers.defilippi.note_application.models.common.BaseDomainModel;
+import lombok.Data;
+
+@Data
+public class CreateNoteRequest {
+    private String title;
+    private String description;
+    private BaseDomainModel category;
 }
