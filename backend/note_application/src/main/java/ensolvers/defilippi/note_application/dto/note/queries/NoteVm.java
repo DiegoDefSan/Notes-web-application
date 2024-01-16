ea7c1fc89cb5f93a6384f06d2e4c5231e011f9c2
DiegoDefSan <defsan35@gmail.com>
2024-01-15 22:31:48 -0500

    Add Note (view mmode) dto

--- backend/note_application/src/main/java/ensolvers/defilippi/note_application/dto/note/queries/NoteVm.java
+++ backend/note_application/src/main/java/ensolvers/defilippi/note_application/dto/note/queries/NoteVm.java
@@ -1,2 +1,13 @@
-package ensolvers.defilippi.note_application.dto.note.queries;public record NoteVm() {
+package ensolvers.defilippi.note_application.dto.note.queries;
+
+import ensolvers.defilippi.note_application.dto.category.queries.CategoryVm;
+import lombok.Data;
+
+@Data
+public class NoteVm {
+    private Long id;
+    private String title;
+    private String description;
+    private boolean isArchived;
+    private CategoryVm category;
 }
