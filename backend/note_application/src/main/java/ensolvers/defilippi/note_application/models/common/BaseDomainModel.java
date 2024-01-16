d9275a27211ade8ff14f8abba208d6de7dd393bd
DiegoDefSan <defsan35@gmail.com>
2024-01-15 22:29:50 -0500

    Add base model
    Abstract class for the id

--- backend/note_application/src/main/java/ensolvers/defilippi/note_application/models/common/BaseDomainModel.java
+++ backend/note_application/src/main/java/ensolvers/defilippi/note_application/models/common/BaseDomainModel.java
@@ -1,2 +1,9 @@
-package ensolvers.defilippi.note_application.models.common;public class BaseDomainModel {
+package ensolvers.defilippi.note_application.models.common;
+
+import lombok.Data;
+import lombok.Getter;
+
+@Data
+public class BaseDomainModel {
+    protected Long id;
 }
