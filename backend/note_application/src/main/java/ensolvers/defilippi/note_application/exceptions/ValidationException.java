1c2b98ca95f7fee15352292fd3fd65d44688d006
DiegoDefSan <defsan35@gmail.com>
2024-01-15 22:35:01 -0500

    Add Validation exception

--- backend/note_application/src/main/java/ensolvers/defilippi/note_application/exceptions/ValidationException.java
+++ backend/note_application/src/main/java/ensolvers/defilippi/note_application/exceptions/ValidationException.java
@@ -1,2 +1,8 @@
-package ensolvers.defilippi.note_application.exceptions;public class ValidationException {
+package ensolvers.defilippi.note_application.exceptions;
+
+public class ValidationException extends RuntimeException {
+
+    public ValidationException(String message) {
+        super(message);
+    }
 }
