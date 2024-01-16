fefc9b6f4d6db7b09943ca735cdf070138dcfffb
DiegoDefSan <defsan35@gmail.com>
2024-01-15 22:34:41 -0500

    Add ErrorMessage model

--- backend/note_application/src/main/java/ensolvers/defilippi/note_application/exceptions/ErrorMessage.java
+++ backend/note_application/src/main/java/ensolvers/defilippi/note_application/exceptions/ErrorMessage.java
@@ -1,2 +1,17 @@
-package ensolvers.defilippi.note_application.exceptions;public class ErrorMessage {
+package ensolvers.defilippi.note_application.exceptions;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ErrorMessage {
+    private int statucCode;
+    private String message;
+    private String description;
+    private LocalDateTime timestamp;
 }
