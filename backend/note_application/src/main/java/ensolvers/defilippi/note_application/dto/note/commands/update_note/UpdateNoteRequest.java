cb21d996cd704eadd806bf70a1c384732ddde405
DiegoDefSan <defsan35@gmail.com>
2024-01-15 22:33:29 -0500

    Add update note commands dto

--- backend/note_application/src/main/java/ensolvers/defilippi/note_application/dto/note/commands/update_note/UpdateNoteRequest.java
+++ backend/note_application/src/main/java/ensolvers/defilippi/note_application/dto/note/commands/update_note/UpdateNoteRequest.java
@@ -1,2 +1,13 @@
-package ensolvers.defilippi.note_application.dto.note.commands.update_note;public record UpdateNoteRequest() {
+package ensolvers.defilippi.note_application.dto.note.commands.update_note;
+
+import ensolvers.defilippi.note_application.models.common.BaseDomainModel;
+import lombok.Data;
+
+@Data
+public class UpdateNoteRequest {
+    private Long id;
+    private String title;
+    private String description;
+    private boolean isArchived;
+    private BaseDomainModel category;
 }
--- backend/note_application/src/main/java/ensolvers/defilippi/note_application/dto/note/commands/update_note/UpdateNoteResponse.java
+++ backend/note_application/src/main/java/ensolvers/defilippi/note_application/dto/note/commands/update_note/UpdateNoteResponse.java
@@ -1,2 +1,13 @@
-package ensolvers.defilippi.note_application.dto.note.commands.update_note;public record UpdateNoteResponse() {
+package ensolvers.defilippi.note_application.dto.note.commands.update_note;
+
+import ensolvers.defilippi.note_application.models.common.BaseDomainModel;
+import lombok.Data;
+
+@Data
+public class UpdateNoteResponse {
+    private Long id;
+    private String title;
+    private String description;
+    private boolean isArchived;
+    private BaseDomainModel category;
 }
