d9fcf1d72cef942db0e7027f1a2f258acfa0472e
DiegoDefSan <defsan35@gmail.com>
2024-01-15 22:37:17 -0500

    Add note repository

--- backend/note_application/src/main/java/ensolvers/defilippi/note_application/repositories/NoteRepository.java
+++ backend/note_application/src/main/java/ensolvers/defilippi/note_application/repositories/NoteRepository.java
@@ -1,4 +1,39 @@
 package ensolvers.defilippi.note_application.repositories;
 
-public interface NoteRepository {
+import ensolvers.defilippi.note_application.models.Note;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+
+public interface NoteRepository extends JpaRepository<Note, Long> {
+
+    @Query("""
+        SELECT n
+        FROM Note n
+        LEFT OUTER JOIN FETCH n.category
+    """)
+    List<Note> selectNotes();
+
+    @Query("""
+        SELECT n
+        FROM Note n
+        LEFT OUTER JOIN FETCH n.category
+        WHERE n.category.id = :categoryId
+    """)
+    List<Note> selectNotesByCategoryId(Long categoryId);
+
+    @Query("""
+        SELECT n
+        FROM Note n
+        WHERE n.isArchived
+    """)
+    List<Note> selectArchivedNotes();
+
+    @Query("""
+        SELECT n
+        FROM Note n
+        WHERE NOT n.isArchived
+    """)
+    List<Note> selectUnarchivedNotes();
 }
