3b1172081b0f58e4d8d49167b142bd2a9c8bd1f7
DiegoDefSan <defsan35@gmail.com>
2024-01-15 22:40:37 -0500

    Modify NoteApp
    Add initial values to database

--- backend/note_application/src/main/java/ensolvers/defilippi/note_application/NoteApplication.java
+++ backend/note_application/src/main/java/ensolvers/defilippi/note_application/NoteApplication.java
@@ -1,13 +1,38 @@
 package ensolvers.defilippi.note_application;
 
+import ensolvers.defilippi.note_application.models.Category;
+import ensolvers.defilippi.note_application.repositories.CategoryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @SpringBootApplication
-public class NoteApplication {
+public class NoteApplication implements org.springframework.boot.CommandLineRunner {
+
+	@Autowired
+	private CategoryRepository categoryRepository;
+
+	private void initCategoryList() {
+		List<Category> categoryList = new ArrayList<>(
+				List.of(
+						new Category("University"),
+						new Category("Work"),
+						new Category("Family")
+				)
+		);
+
+		categoryRepository.saveAll(categoryList);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		initCategoryList();
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(NoteApplication.class, args);
 	}
-
 }
