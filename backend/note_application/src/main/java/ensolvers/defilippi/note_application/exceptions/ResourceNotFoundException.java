a4804b2623aa3fc3928feb4a20dfb6a8efdb3aae
DiegoDefSan <defsan35@gmail.com>
2024-01-15 22:35:14 -0500

    Add resource not found exception

--- backend/note_application/src/main/java/ensolvers/defilippi/note_application/exceptions/ResourceNotFoundException.java
+++ backend/note_application/src/main/java/ensolvers/defilippi/note_application/exceptions/ResourceNotFoundException.java
@@ -1,2 +1,7 @@
-package ensolvers.defilippi.note_application.exceptions;public class ResourceNotFoundException {
+package ensolvers.defilippi.note_application.exceptions;
+
+public class ResourceNotFoundException extends RuntimeException {
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
 }
