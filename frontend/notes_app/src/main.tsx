c3b142754bdcdf2e805fc5ca4a33f2b04aad8d9a
DiegoDefSan <defsan35@gmail.com>
2024-01-17 17:22:02 -0500

    ref: main app

--- frontend/notes_app/src/main.tsx
+++ frontend/notes_app/src/main.tsx
@@ -1,10 +1,29 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
+import { App } from './App.tsx'
 import './index.css'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { PopFormProvider } from './contexts/PopFormContext.tsx'
+import { ModifyingNoteProvider } from './contexts/ModifyingNoteContext.tsx'
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: 3,
+      refetchOnWindowFocus: false,
+      staleTime: 1000 * 60 * 5, // 5 minutes
+    },
+  },
+})
+
+ReactDOM.createRoot(document.getElementById('root')! as HTMLElement).render(
+  <ModifyingNoteProvider>
+    <PopFormProvider>
+      <React.StrictMode>
+        <QueryClientProvider client={queryClient}>
+          <App />
+        </QueryClientProvider>
+      </React.StrictMode>
+    </PopFormProvider>
+  </ModifyingNoteProvider>
 )
