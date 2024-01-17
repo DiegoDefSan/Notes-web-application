57ed9412c49ea439f85046a5b9807902251d4a47
DiegoDefSan <defsan35@gmail.com>
2024-01-17 14:28:09 -0500

    feat: category service implementation

--- backend/note_application/src/main/java/ensolvers/defilippi/note_application/services/impl/CategoryService.java
+++ backend/note_application/src/main/java/ensolvers/defilippi/note_application/services/impl/CategoryService.java
@@ -1,2 +1,32 @@
-package ensolvers.defilippi.note_application.services.impl;public class CategoryService {
+package ensolvers.defilippi.note_application.services.impl;
+
+import ensolvers.defilippi.note_application.dto.category.queries.CategoryVm;
+import ensolvers.defilippi.note_application.models.Category;
+import ensolvers.defilippi.note_application.repositories.CategoryRepository;
+import ensolvers.defilippi.note_application.services.ICategoryService;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CategoryService implements ICategoryService {
+
+    @Autowired
+    private CategoryRepository categoryRepository;
+
+    @Autowired
+    private ModelMapper modelMapper;
+
+    @Override
+    public List<CategoryVm> getCategories() {
+        List<Category> categoryList = categoryRepository.findAll();
+
+        List<CategoryVm> categoryVmList = categoryList.stream().map(
+                category -> modelMapper.map(category, CategoryVm.class)
+        ).toList();
+
+        return categoryVmList;
+    }
 }
