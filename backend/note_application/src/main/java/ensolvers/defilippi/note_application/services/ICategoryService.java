32637a3eccf8d7fb6806eed456a9d1e224c25060
DiegoDefSan <defsan35@gmail.com>
2024-01-17 14:27:55 -0500

    feat: category service interface

--- backend/note_application/src/main/java/ensolvers/defilippi/note_application/services/ICategoryService.java
+++ backend/note_application/src/main/java/ensolvers/defilippi/note_application/services/ICategoryService.java
@@ -1,2 +1,9 @@
-package ensolvers.defilippi.note_application.services;public interface ICategoryService {
+package ensolvers.defilippi.note_application.services;
+
+import ensolvers.defilippi.note_application.dto.category.queries.CategoryVm;
+
+import java.util.List;
+
+public interface ICategoryService {
+    List<CategoryVm> getCategories();
 }
