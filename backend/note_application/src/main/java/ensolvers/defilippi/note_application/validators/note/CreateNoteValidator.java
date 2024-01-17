7816cdcc39533af782313c5686c0a76e43e2d673
DiegoDefSan <defsan35@gmail.com>
2024-01-17 17:24:55 -0500

    backend v_1
    validators modified

--- backend/note_application/src/main/java/ensolvers/defilippi/note_application/validators/note/CreateNoteValidator.java
+++ backend/note_application/src/main/java/ensolvers/defilippi/note_application/validators/note/CreateNoteValidator.java
@@ -11,16 +11,8 @@ public final class CreateNoteValidator {
             throw new ValidationException("Note title is required");
         }
 
-        if (note.getTitle().length() > 16) {
-            throw new ValidationException("Note title cannot exceed " + maxTitleCharacters + " characters");
-        }
-
         if (note.getDescription() == null || note.getDescription().isEmpty()) {
             throw new ValidationException("Note description is required");
-        }
-
-        if (note.getDescription().length() > 64) {
-            throw new ValidationException("Note description cannot exceed " + maxDescriptionCharacters + " characters");
         }
 
         if (note.getCategory() == null) {
--- backend/note_application/src/main/java/ensolvers/defilippi/note_application/validators/note/UpdateNoteValidator.java
+++ backend/note_application/src/main/java/ensolvers/defilippi/note_application/validators/note/UpdateNoteValidator.java
@@ -16,16 +16,8 @@ public class UpdateNoteValidator {
             throw new ValidationException("Note title is required");
         }
 
-        if (note.getTitle().length() > 16) {
-            throw new ValidationException("Note title cannot exceed " + maxTitleCharacters + " characters");
-        }
-
         if (note.getDescription() == null || note.getDescription().isEmpty()) {
             throw new ValidationException("Note description is required");
-        }
-
-        if (note.getDescription().length() > 64) {
-            throw new ValidationException("Note description cannot exceed " + maxDescriptionCharacters + " characters");
         }
 
         if (note.getCategory() == null) {
