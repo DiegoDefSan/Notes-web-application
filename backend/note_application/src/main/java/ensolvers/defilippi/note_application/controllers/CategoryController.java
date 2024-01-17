cd6bd923c82efe3b3cd74797d5db34683d281ef0
DiegoDefSan <defsan35@gmail.com>
2024-01-17 14:28:29 -0500

    feat: category controller added

--- backend/note_application/src/main/java/ensolvers/defilippi/note_application/controllers/CategoryController.java
+++ backend/note_application/src/main/java/ensolvers/defilippi/note_application/controllers/CategoryController.java
@@ -1,2 +1,28 @@
-package ensolvers.defilippi.note_application.controllers;public class CategoryController {
+package ensolvers.defilippi.note_application.controllers;
+
+import ensolvers.defilippi.note_application.dto.category.queries.CategoryVm;
+import ensolvers.defilippi.note_application.services.impl.CategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@CrossOrigin("http://localhost:5173")
+@RestController
+@RequestMapping("/api/v1/notes/categories") // http://localhost:8080//api/v1/notes/categories
+public class CategoryController {
+    @Autowired
+    private CategoryService categoryService;
+
+    @GetMapping
+    public ResponseEntity<List<CategoryVm>> getCategories() {
+        List<CategoryVm> categoriesFromService = categoryService.getCategories();
+
+        return new ResponseEntity<>(categoriesFromService, HttpStatus.OK);
+    }
 }
